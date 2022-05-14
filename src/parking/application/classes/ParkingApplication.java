3b5b3ee4e6476e210337d6cb1e605c6e1f857504
unknown <moresayed191@gmail.com>
2022-05-14 18:04:49 +0200

    src Removed!

--- src/parking/application/classes/ParkingApplication.java
+++
@@ -1,8 +0,0 @@
-package parking.application.classes;
-
-public class  ParkingApplication{
-
-    public static void main(String[] args) throws ClassNotFoundException {
-        Connectsql.setConnection();
-    }
-}
